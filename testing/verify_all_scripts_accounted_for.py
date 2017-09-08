9a1bfe7a2eb258712e9b974bff1e8c3c92fba6af
Andrew Leaver-Fay <aleaverfay@gmail.com>
2017-09-08 12:59:13 -0400

    OK -- results of the verify_all_scripts_accounted_for is now written to a .json file

--- testing/verify_all_scripts_accounted_for.py
+++ testing/verify_all_scripts_accounted_for.py
@@ -8,30 +8,49 @@
 # 1. all scripts listed in the three files above are found in the source directory, and
 # 2. all the xml files that are found have been listed in at least one of the above scripts
 
-import os, sys
+import os, sys, subprocess
 
 from untested_scripts import *
 from scripts_to_validate import *
 from scripts_to_parse import *
 
+import json
+
 all_listed_scripts = set( scripts_to_be_validated + scripts_to_be_parsed + scripts_not_tested )
 all_found_files = set( filter( lambda x : len(x) >= 4 and x[-4:] == ".xml", [ os.path.join( x[0], y ) for x in os.walk( "scripts" ) for y in x[2] ] ))
 
-for x in all_found_files :
-    print( "Found: " + x )
+#for x in all_found_files :
+#    print( "Found: " + x )
 
+scripts_not_found = []
+scripts_not_listed = {}
 
 all_good = True
 for script in all_listed_scripts :
     if script not in all_found_files :
         all_good = False
-        print( "Script '" + script + "' was not found in the source tree" )
+        scripts_not_found.append( script )
 
 for script in all_found_files :
     if script not in all_listed_scripts :
         all_good = False
-        print( "Script '" + script + "' was not listed in any of the three files listing what scripts to test and how" )
+        blame_command = ( "git blame " + script ).split()
+        child = subprocess.Popen( blame_command, stdout=subprocess.PIPE )
+        blame_out, blame_err = child.communicate()
+        blame_lines = blame_out.split("\n")
+        last_line = blame_lines[-2]
+        
+        author = last_line[ (last_line.find("(")+1) : last_line.find( ")") ].split("20")[0] # Y3K Bug here!
+        #print( last_line )
+        #print( author )
+        scripts_not_listed[ script ] = author
+        
+json.dump(
+    { "result" : all_good, "scripts_not_found" : scripts_not_found, "scripts_not_listed" : scripts_not_listed },
+    open( "verify_all_scripts_accounted_for_result.json", 'w' ),
+    sort_keys = True )
 
-if all_good : sys.exit(0)
-else :        sys.exit(1)
+
+
+
 
