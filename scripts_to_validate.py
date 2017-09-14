3fa2cb0557c2bdfeadb426bd97478a561c6f6ac3
Andrew Leaver-Fay <aleaverfay@gmail.com>
2017-09-14 16:34:04 -0400

    Adding a pair of feature-analysis scripts

--- scripts_to_validate.py
+++ scripts_to_validate.py
@@ -8,5 +8,7 @@ scripts_to_be_validated = [
     "scripts/pilot/example/an_example_for_testing_purposes/example_valid.xml",
     "scripts/public/sewing/refinement/zn_refinement.xml",
     "scripts/public/rotamer_recovery/ex1ex2_repack_t4400lt200_betaNov15_npdhb/rotamer_recovery_via_feature_analysis.xml",
+    "scripts/public/feature_analysis/interface_feature_analysis/interface_features.xml",
+    "scripts/public/feature_analysis/hbond_features_from_relaxed_decoys_npdhb_potential/hbond_feature_extraction.xml",
 
 ]
