239abb50a38abc16811b15fbab519077037ae0d1
Daniel Farrell <danpf@uw.edu>
2017-10-03 18:57:48 -0700

    Added quick relax into density script
    Added a script to relax a homology model (or whatever your heart desires)
    into density.

--- scripts_to_parse.py
+++ scripts_to_parse.py
@@ -15,6 +15,7 @@ scripts_to_be_parsed = [
 "scripts/public/protein_hacking/helix_extend_with_threading.xml",
 "scripts/public/loop_modeling/simple_loop_grower/simple_loop_grower.xml",
 "scripts/public/protein_hacking/helix_extender.xml",
-    "scripts/pilot/symmetry/symm.xml",
+"scripts/pilot/symmetry/symm.xml",
+"scripts/public/homology_modeling/relax_into_density/relax_into_density.xml"
 ]
 #note - do not put a trailing comma on the closing ] if you are doing multiline editing to add the trailing commas to the 10 scripts you just added - it will convert the list to a tuple and blow up the world
