30c83b3aee503705b8661704cdbc501403b4c071
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2018-05-08 18:13:36 -0400

    Promoted a few more scripts from "validate" to "parse."

--- scripts_to_parse.py
+++ scripts_to_parse.py
@@ -47,6 +47,11 @@ scripts_to_be_parsed = [
 	"scripts/public/sewing/refinement/HR1B_refinement.xml",
     "scripts/public/sewing/refinement/HR1B_refinement_remove_partner.xml",
 	"scripts/public/sewing/refinement/refine_C-terminal_binding_helix.xml",
-	"scripts/public/visualize_protocol/template_pymol_mover.xml"
+	"scripts/public/visualize_protocol/template_pymol_mover.xml",
+	"scripts/pilot/protein_interface_design/edge_strand_mediated_interface_design/relaxed_tfr_retry_nonrelaxed_DeNovo_scaffolds_DeNovoStrands_add_2nd_helix_back_designs_bpb2_csts.xml",
+	"scripts/pilot/protein_interface_design/edge_strand_mediated_interface_design/relaxed_tfr_retry_nonrelaxed_DeNovo_scaffolds_DeNovoStrands_add_2nd_helix_back_designs_c_pass_ba.bp_pass__20161129210609.pdb.bp_20161219173455_0001_0002_bpb2.xml",
+	"scripts/pilot/protein_interface_design/edge_strand_mediated_interface_design/relaxed_tfr_retry_nonrelaxed_DeNovo_scaffolds_DeNovoStrands_add_2nd_helix_back_designs_c_pass_gba.bp_pass__20161129210254.pdb.bp_20161219172548_0001_0003_bpb2.xml",
+	"scripts/pilot/protein_interface_design/edge_strand_mediated_interface_design/relaxed_tfr_retry_nonrelaxed_DeNovo_scaffolds_DeNovoStrands_add_2nd_helix_back_designs_c_pass_gba.bp_pass__20161129210726.pdb.bp_20161219173002_0001_0001_bpb2.xml",
+	"scripts/pilot/protein_interface_design/edge_strand_mediated_interface_design/relaxed_tfr_retry_nonrelaxed_DeNovo_scaffolds_DeNovoStrands_add_2nd_helix_back_designs_c_pass_gba.bp_pass__20161129210228.pdb.bp_20161219172446_0001_0001_bpb2.xml"
 ]
 #note - do not put a trailing comma on the closing ] if you are doing multiline editing to add the trailing commas to the 10 scripts you just added - it will convert the list to a tuple and blow up the world
