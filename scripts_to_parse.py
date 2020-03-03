6eccb4124052150f95f29d6373318ce3e5b113dd
Jack Maguire <jack@med.unc.edu>
2020-03-03 16:48:46 -0500

    Added scripts/public/point_mutant_scan/point_mutant_scan.xml to lists

--- scripts_to_parse.py
+++ scripts_to_parse.py
@@ -15,6 +15,7 @@ scripts_to_be_parsed = [
     "scripts/public/protein_hacking/helix_extend_with_threading.xml",
     "scripts/public/loop_modeling/simple_loop_grower/simple_loop_grower.xml",
     "scripts/public/protein_hacking/helix_extender.xml",
+    "scripts/public/point_mutant_scan/point_mutant_scan.xml",
     "scripts/pilot/symmetry/symm.xml",
     "scripts/public/homology_modeling/relax_into_density/relax_into_density.xml",
     "scripts/pilot/enzymedesign/proenzyme_design/cpg2_proenzyme_prodomain_energylandscape.xml",
